b139a718d8551ad904979201457509a0542ed8f1
SimonRoG <prokhodasemen@gmail.com>
2024-09-02 15:44:19 +0200

    .

---
+++ app.py
@@ -0,0 +1,67 @@
+from flask import *
+import json
+import os
+
+app = Flask(__name__)
+
+
+def render_template_(html, **context):
+    return render_template(html, menu_bar="menuBar.html", **context)
+
+
+@app.route("/")
+def index():
+    return render_template_("index.html")
+
+
+@app.route("/Impressum")
+def impressum():
+    return render_template_("impressum.html")
+
+
+@app.route("/Datenschutz")
+def datenschutz():
+    return render_template_("datenschutz.html")
+
+
+@app.template_filter('format_number')
+def format_number(value):
+    return f"{int(value):_}".replace("_", " ")
+
+@app.route("/Referenzen")
+def referenzen():
+    with open(
+        os.path.join(app.root_path, "data", "Referenzen.json"),
+        "r",
+        encoding="utf-8-sig",
+    ) as f:
+        data = json.load(f)
+
+    return render_template_("referenzen.html", data=data)
+
+
+@app.route("/Referenzen/<Projekt>")
+def referenz(Projekt):
+    return render_template_("projekt.html")
+
+
+@app.route("/Karriere")
+def karriere():
+    with open(
+        os.path.join(app.root_path, "data", "Stellenanzeigen.json"),
+        "r",
+        encoding="utf-8",
+    ) as f:
+        data = json.load(f)
+
+    return render_template_("karriere.html", data=data)
+
+
+@app.route("/Geschaeftsfelder/<Geschaeftsfeld>")
+def geschaeftsfelder(Geschaeftsfeld):
+    return render_template_(f"Geschaeftsfelder/{Geschaeftsfeld}.html")
+
+
+if __name__ == "__main__":
+    app.config["SERVER_NAME"] = "semen.net:666"
+    app.run()
---
+++ static/BAE.js
@@ -0,0 +1,153 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var loadingPage = document.getElementById("loadingPage");
+    var loadingLogoContainer = document.getElementById("loadingLogoContainer");
+    var startPage = document.getElementById("startPage");
+    var videoContainer = document.querySelector(".Hauptseite");
+    var videoBackground = document.getElementById("video-background");
+    var loadingSpinner = document.getElementById("loadingSpinner");
+
+    function showLogoAndText() {
+        if (loadingLogoContainer) {
+            loadingLogoContainer.style.opacity = 1;
+        }
+
+        setTimeout(function () {
+            if (loadingSpinner) {
+                loadingSpinner.style.display = "block";
+            }
+            if (document.body) {
+                document.body.classList.remove("loading");
+            }
+        }, 50);
+    }
+
+    showLogoAndText();
+
+    function setupPageTransition() {
+        if (loadingSpinner) {
+            loadingSpinner.style.display = "none";
+        }
+        if (loadingPage) {
+            loadingPage.style.display = "none";
+        }
+        if (startPage) {
+            startPage.style.display = "none";
+        }
+        if (videoContainer) {
+            videoContainer.style.display = "block";
+        }
+    }
+
+    if (document.body.id === "home-page") {
+        videoBackground.addEventListener('canplay', function () {
+            setupPageTransition(); 
+            videoBackground.addEventListener('canplaythrough', function () {
+                videoBackground.play();
+            });
+        });
+    } else {
+        setupPageTransition();
+    }
+
+    window.addEventListener("scroll", function () {
+        var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+        if (videoContainer) {
+            var videoOffset = videoContainer.offsetTop;
+
+            if (scrollPosition < videoOffset && videoBackground && !videoBackground.paused) {
+                var menuBarOverlay = document.getElementById("menuBarOverlay");
+                if (menuBarOverlay) {
+                    menuBarOverlay.style.display = "block";
+                }
+            }
+        }
+    });
+});
+
+function closeMobileMenu() {
+    document.getElementById("mobileMenu").classList.remove('down');
+    setTimeout(() => {
+        document.getElementById("mobileMenu").style.display = "none";
+    }, 700);
+}
+
+function openMobileMenu() {
+    document.getElementById("mobileMenu").style.display = "block";
+    setTimeout(() => {
+        document.getElementById("mobileMenu").classList.add('down');
+    }, 50);
+}
+
+function popup(img) {
+    window.open(
+        img,
+        'window',
+        'toolbar=no, location=no, status=no, menubar=no, scrollbars=yes, resizable=yes, width=1090px, height=550px, top=25px left=120px'
+    );
+}
+
+function filterApply() {
+    let ort = document.getElementById('ort').value;
+    let anstellung = document.getElementById('anstellung').value;
+    let taetigkeit = document.getElementById('taetigkeit').value;
+    let stellenanzeigen = Array.from(document.getElementsByClassName('stellenanzeige'));
+
+    stellenanzeigen.forEach(stellenanzeige => {
+        stellenanzeige.style.display = 'none';
+        if (stellenanzeige.classList.contains(ort) &&
+            stellenanzeige.classList.contains(anstellung) &&
+            stellenanzeige.classList.contains(taetigkeit)) {
+            stellenanzeige.style.display = 'block';
+        }
+    });
+}
+
+function displaySelected() {
+    const seiten = document.getElementsByClassName('seite');
+    const projekteArr = document.getElementsByClassName('projekt')
+    let index;
+    for (let i = 0; i < seiten.length; i++)
+        if (seiten[i].classList.contains('selected'))
+            index = i;
+
+    for (let i = 0; i < projekteArr.length; i++)
+        if (i >= index * 10 && i < (index + 1) * 10)
+            projekteArr[i].style.display = 'flex';
+        else
+            projekteArr[i].style.display = 'none';
+    window.scrollTo(0, 0);
+}
+
+function navigate(index) {
+    const seiten = document.getElementsByClassName('seite');
+    for (let i = 0; i < seiten.length; i++)
+        if (seiten[i].classList.contains('selected'))
+            seiten[i].classList.remove('selected');
+
+    const seite = document.getElementById('seite' + index);
+    seite.classList.add('selected');
+    displaySelected();
+
+}
+
+function prev() {
+    const seiten = document.getElementsByClassName('seite');
+    for (let i = 0; i < seiten.length; i++)
+        if (seiten[i].classList.contains('selected') && i != 0) {
+            seiten[i].classList.remove('selected');
+            seiten[i - 1].classList.add('selected');
+            displaySelected();
+            return;
+        }
+}
+
+function next() {
+    const seiten = document.getElementsByClassName('seite');
+    for (let i = 0; i < seiten.length; i++)
+        if (seiten[i].classList.contains('selected') && i != seiten.length - 1) {
+            seiten[i].classList.remove('selected');
+            seiten[i + 1].classList.add('selected');
+            displaySelected();
+            return;
+        }
+}
