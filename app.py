f4067dd1efc98065739d95dfb179ca68eb0a1857
SimonRoG <prokhodasemen@gmail.com>
2024-10-29 15:52:05 +0100

    en version text

--- app.py
+++ app.py
@@ -20,19 +20,27 @@ with open(os.path.join(app.root_path, "data", "keys.json"), "r") as file:
 
 RECAPTCHA_SITE_KEY = keys["RECAPTCHA_SITE_KEY"]
 RECAPTCHA_SECRET_KEY = keys["RECAPTCHA_SECRET_KEY"]
-app.config['RECAPTCHA_PUBLIC_KEY'] = RECAPTCHA_SITE_KEY
-app.config['RECAPTCHA_PRIVATE_KEY'] = RECAPTCHA_SECRET_KEY
+app.config["RECAPTCHA_PUBLIC_KEY"] = RECAPTCHA_SITE_KEY
+app.config["RECAPTCHA_PRIVATE_KEY"] = RECAPTCHA_SECRET_KEY
+
 
 class Formular(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    email = EmailField('Email', validators=[DataRequired()])
-    massage = TextAreaField('Massage')
-    file = FileField('File', validators=[
-        FileRequired(),
-        FileAllowed(['pdf'], '*.pdf only!')
-        ])
-    recaptcha = RecaptchaField('reCAPTCHA', validators=[DataRequired()])
-    submit = SubmitField('Senden')
+    name = StringField("Name", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired()])
+    massage = TextAreaField("Massage")
+    file = FileField(
+        "File", validators=[FileRequired(), FileAllowed(["pdf"], "*.pdf only!")]
+    )
+    recaptcha = RecaptchaField("reCAPTCHA", validators=[DataRequired()])
+    submit = SubmitField("Senden")
+
+
+def language(html):
+    route = request.path
+    if "/en/" in route:
+        html = "en/" + html
+    return html
+
 
 with open(
     os.path.join(app.root_path, "data", "Referenzen.json"),
@@ -42,28 +50,45 @@ with open(
     Referenzen = json.load(file)
 
 with open(
+    os.path.join(app.root_path, "data", "en/Referenzen.json"),
+    "r",
+    encoding="utf-8-sig",
+) as file:
+    Referenzen_en = json.load(file)
+
+with open(
     os.path.join(app.root_path, "data", "Stellenanzeigen.json"),
     "r",
     encoding="utf-8",
 ) as file:
     Stellenanzeigen = json.load(file)
 
+with open(
+    os.path.join(app.root_path, "data", "en/Stellenanzeigen.json"),
+    "r",
+    encoding="utf-8",
+) as file:
+    Stellenanzeigen_en = json.load(file)
+
 
 def render_template_(html, **context):
-    return render_template(html, menu_bar="menuBar.html", **context)
+    return render_template(language(html), menu_bar=language("menuBar.html"), **context)
 
 
 @app.route("/")
+@app.route("/en/")
 def index():
     return render_template_("index.html")
 
 
 @app.route("/Impressum")
+@app.route("/en/Impressum")
 def impressum():
     return render_template_("impressum.html")
 
 
 @app.route("/Datenschutz")
+@app.route("/en/Datenschutz")
 def datenschutz():
     return render_template_("datenschutz.html")
 
@@ -74,38 +99,67 @@ def format_number(value):
 
 
 @app.route("/Referenzen")
+@app.route("/en/Referenzen")
 def referenzen():
-    return render_template_("referenzen.html", data=Referenzen)
+    route = request.path
+    data = Referenzen
+    if "/en/" in route:
+        data = Referenzen_en
+
+    return render_template_("referenzen.html", data=data)
 
 
 @app.route("/Referenzen/<Projekt>")
+@app.route("/en/Referenzen/<Projekt>")
 def projekt(Projekt):
-    for item in Referenzen:
+    route = request.path
+    data = Referenzen
+    if "/en/" in route:
+        data = Referenzen_en
+
+    for item in data:
         if item["Bild"][:-4] == Projekt:
             return render_template_("projekt.html", item=item)
 
 
 @app.route("/Karriere")
+@app.route("/en/Karriere")
 def karriere():
-    return render_template_("karriere.html", data=Stellenanzeigen)
+    route = request.path
+    data = Stellenanzeigen
+    if "/en/" in route:
+        data = Stellenanzeigen_en
+
+    return render_template_("karriere.html", data=data)
 
 
 @app.route("/Karriere/<Stelle>")
+@app.route("/en/Karriere/<Stelle>")
 def stelle(Stelle):
     form = Formular()
 
-    for item in Stellenanzeigen:
+    route = request.path
+    data = Stellenanzeigen
+    if "/en/" in route:
+        data = Stellenanzeigen_en
+
+    for item in data:
         if item["Name"][:-7] == Stelle:
             return render_template_(
-                "stelle.html", item=item, recaptcha_site_key=RECAPTCHA_SITE_KEY, form=form
+                "stelle.html",
+                item=item,
+                recaptcha_site_key=RECAPTCHA_SITE_KEY,
+                form=form,
             )
 
 
 @app.route("/Geschaeftsfelder/<Geschaeftsfeld>")
+@app.route("/en/Geschaeftsfelder/<Geschaeftsfeld>")
 def geschaeftsfelder(Geschaeftsfeld):
     return render_template_(f"Geschaeftsfelder/{Geschaeftsfeld}.html")
 
 
 @app.route("/Bilder/<Bild>")
+@app.route("/en/Bilder/<Bild>")
 def bild(Bild):
     return send_from_directory("static/Bilder", Bild)
