ada077269e85bd433fd9cdfa0c3f92c1cc60af0a
Semen Prokhoda <prokhodasemen@gmail.com>
2025-05-08 16:57:36 +0200

    add logging

--- start.py
+++ start.py
@@ -1,4 +1,16 @@
 from app import app
+import logging
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+# Set up logging
+logging.basicConfig(
+    filename="flask_app.log",  # Log file name
+    level=logging.INFO,  # Logging level
+    format="%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]",
+)
+
+# Optionally redirect Flask's default logger to file
+app.logger.addHandler(logging.FileHandler("flask_app.log"))
+app.logger.setLevel(logging.INFO)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
